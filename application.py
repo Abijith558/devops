d8bb2da2196a73cd8a14a409544feafeceafa555
EC2 Default User <ec2-user@ip-172-31-10-48.eu-west-1.compute.internal>
2023-12-11 16:09:04 +0000

    changes

--- application.py
+++ application.py
@@ -1,21 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
-db = SQLAlchemy(app)
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///notes.db'
+db = SQLAlchemy(application)
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
 
-@app.route('/')
+@application.route('/')
 def index():
     notes = Note.query.all()
     return render_template('index.html', notes=notes)
 
-@app.route('/add_note', methods=['POST'])
+@application.route('/add_note', methods=['POST'])
 def add_note():
     title = request.form['title']
     content = request.form['content']
@@ -24,7 +24,7 @@ def add_note():
     db.session.commit()
     return redirect(url_for('index'))
 
-@app.route('/edit_note/<int:id>', methods=['GET', 'POST'])
+@application.route('/edit_note/<int:id>', methods=['GET', 'POST'])
 def edit_note(id):
     note = Note.query.get(id)
     if request.method == 'POST':
@@ -34,7 +34,7 @@ def edit_note(id):
         return redirect(url_for('index'))
     return render_template('edit.html', note=note)
 
-@app.route('/delete_note/<int:id>')
+@application.route('/delete_note/<int:id>')
 def delete_note(id):
     note = Note.query.get(id)
     db.session.delete(note)
@@ -42,7 +42,7 @@ def delete_note(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    with app.app_context():
+    with application.app_context():
         db.create_all()
-    app.run(debug=True)
+    application.run(debug=True)
 
